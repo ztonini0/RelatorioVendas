34d2d233430bd1d41f63af935cff4fb68b5467bb
ztonin0 <contatoisthemat@outlook.com>
2021-10-27 17:30:27 -0300

    Adicionando main.py

---
+++ main.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import win32com.client as win32
+from twilio.rest import Client
+
+account_sid = "ACb38a8a2c6954af7d0f57371d54e98fb8"
+auth_token = "6b75bf73d9224c268393754ca5c322e9"
+client = Client(account_sid, auth_token)
+
+
+# importar a base de dados
+tabela_vendas = pd.read_excel('Vendas.xlsx')
+
+# visualizar a base de dados
+pd.set_option('display.max_columns', None)
+print(tabela_vendas)
+
+# faturamento por loja
+faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
+print(faturamento)
+
+
+# quantidade de produtos vendidos por loja
+quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+print(quantidade)
+
+print('-' * 50)
+# ticket médio por produto em cada loja
+ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
+ticket_medio = ticket_medio.rename(columns={0: 'Ticket Médio'})
+print(ticket_medio)
+
+# enviar um email com o relatório
+outlook = win32.Dispatch('outlook.application')
+mail = outlook.CreateItem(0)
+mail.To = 'pythontesting200w@gmail.com'
+mail.Subject = 'Relatório de Vendas por Loja'
+mail.HTMLBody = f'''
+<p>Prezado, Matheus Tonini</p>
+
+<p>A seguir Relatório de Vendas por cada Loja.</p>
+
+<p>Faturamento:</p>
+{faturamento.to_html(formatters={'Valor Final': 'R${:,.2f}'.format})}
+
+<p>Quantidade Vendida:</p>
+{quantidade.to_html()}
+
+<p>Ticket Médio dos Produtos em cada Loja:</p>
+{ticket_medio.to_html(formatters={'Ticket Médio': 'R${:,.2f}'.format})}
+
+<p>Qualquer dúvida estou à disposição.</p>
+
+<p>Att.,</p>
+<p>Matheus Tonini</p>
+'''
+
+mail.Send()
+
+message = client.messages.create(
+to= "+5511934986689",
+from_ = "+12674353154",
+body=f'Prezado, Matheus Tonini, o foi Relatório enviado com sucesso para seu email.')
+print(message.sid)
+
